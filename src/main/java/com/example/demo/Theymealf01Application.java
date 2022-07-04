ff3ef429da2dbcbd8e4587e5f94ff98c1024299b
Laxmikanta2020 <akash83388@gmail.com>
2022-07-04 06:48:10 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Theymealf01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Theymealf01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Theymealf01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/rest/RestControllerExample.java
@@ -0,0 +1,18 @@
+package com.example.demo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestControllerExample {
+
+	
+	@GetMapping("/laxmi")
+	public String Example( Model model) {
+		
+		String s="Hi laxmikanta you are lifesever";
+		model.addAttribute("msg",s);
+		return "laxmi";
+	}
+}
