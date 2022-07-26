4eb34c190b9319d5f4996fa72268b160c0e6a58a
SoumyaL2 <soumyapanda834@gmail.com>
2022-07-26 13:42:27 +0530

    done

--- src/main/java/com/example/demo/rest/RestControllerExample.java
+++ src/main/java/com/example/demo/rest/RestControllerExample.java
@@ -11,4 +11,7 @@ public class RestControllerExample {
 	String l="Hi Luisa";
 	int i=96;
 	String l2="we are pleased to inform u that u are recently joined our company";
+	String l3="Hi Akash you are also selcted for GOOGLE";
+	int i2=90;
+	String l4="offred salary is 15lpa";
 }
