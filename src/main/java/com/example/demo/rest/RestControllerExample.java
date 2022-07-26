e8b0f170bcbbeba3b093e7da5b759408b2940021
Laxmikanta2020 <akash83388@gmail.com>
2022-07-26 13:37:21 +0530

    laxmi

--- src/main/java/com/example/demo/rest/RestControllerExample.java
+++ src/main/java/com/example/demo/rest/RestControllerExample.java
@@ -1,5 +1,7 @@
 package com.example.demo.rest;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,4 +13,12 @@ public class RestControllerExample {
 	String l="Hi Luisa";
 	int i=96;
 	String l2="we are pleased to inform u that u are recently joined our company";
+	public static void abc() throws IOException {
+		throw new IOException("sorry 4o4 error");   
+		
+	}
+	public static void abc1() {
+		throw new ArithmeticException("sorry ............. error....................");   
+		
+	}
 }
