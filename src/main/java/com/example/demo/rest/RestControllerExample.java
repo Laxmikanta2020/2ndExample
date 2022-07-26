741dd824ce179f0098c6f63a3f61bf7feaef05ea
SoumyaL2 <soumyapanda834@gmail.com>
2022-07-26 13:16:25 +0530

    luisa

--- src/main/java/com/example/demo/rest/RestControllerExample.java
+++ src/main/java/com/example/demo/rest/RestControllerExample.java
@@ -8,28 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RestControllerExample {
 
 
-	String s="Hi laxmikanta you are lifesever";
-
-	String ss="Hi laxmikanta";
-
-	int a=100;
-	int b=200;
-
-	String laxmi="laxmikanta";
-	double d=100000.99;
-
-	@GetMapping("/laxmi")
-	public String Example( Model model) {
-		
-		String s="Hi laxmikanta you are lifesever";
-		model.addAttribute("msg",s);
-		return "laxmi";
-	}
-	@GetMapping("/laxmiKanta")
-	public String ExampleModel( Model model) {
-		
-		String s="Hi laxmikantaWelcome to Our Company..";
-		model.addAttribute("msg",s);
-		return "laxmi";
-	}
+	String l="Hi Luisa";
+	int i=96;
+	String l2="we are pleased to inform u that u are recently joined our company";
 }
